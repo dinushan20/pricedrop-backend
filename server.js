dd86a89a51bd304b9fcfe06ff861de4c4790cb4c
Dinushan Meeriyagalla <73055030+dinushan20@users.noreply.github.com>
2025-04-29 11:16:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 // Handle unexpected errors
 process.on('uncaughtException', (err) => {
